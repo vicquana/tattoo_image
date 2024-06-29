a6fb7221a5d1d4cd9f4a67f2630e43659742143b
zenfqua <zenfqua@gmail.com>
2024-06-29 13:12:01 +0800

    exclude __pyche__ when updating biography

--- update_biography.py
+++ update_biography.py
@@ -1,6 +1,7 @@
 import os
+import time
+import random
 import instaloader
-
 
 def update_biography():
     # Function to get all subfolder names in the directory of update_biography.py
@@ -8,7 +9,7 @@ def update_biography():
     target_folder = os.path.join(directory, '')
 
     def get_subfolder_names(directory):
-        return [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name)) and not name.startswith('.')]
+        return [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name)) and not name.startswith('.') and not name.startswith('_')]
 
     def download_biography(username, folder_path):
         L = instaloader.Instaloader()
@@ -26,9 +27,12 @@ def update_biography():
             subfolder_path = os.path.join(target_folder, subfolder_name)
             biography = download_biography(subfolder_name, subfolder_path)
             print(f"Biography of {subfolder_name} saved")
+            # Introduce a random delay to avoid request frequency restriction
+            delay = random.randint(30, 90)  # Random delay between 30 and 90 seconds
+            print(f"Waiting for {delay} seconds before the next request...")
+            time.sleep(delay)
         except Exception as e:
             print(f"An error occurred while processing {subfolder_name}: {str(e)}")
-
 
 if __name__ == "__main__":
     update_biography()
